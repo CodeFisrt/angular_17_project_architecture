21996740884a76e45c687eb1dfa156b2dadd50de
Vinoth <vinosvpr@gmail.com>
2024-04-10 11:57:22 +0530

    Adding category component detail

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+  styleUrl: './category.component.css',
 })
-export class CategoryComponent {
-
+export class CategoryComponent implements OnInit {
+  categoryList: any[] = [];
+  isLoaderPresent: boolean = true;
+  catObj: any = {
+    CategoryId: 0,
+    CategoryName: '',
+    ParentCategoryId: 0,
+  };
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.getCategory();
+  }
+  getCategory() {
+    this.http
+      .get('https://freeapi.gerasim.in/api/BigBasket/GetAllCategory')
+      .subscribe((res: any) => {
+        this.categoryList = res.data;
+        this.isLoaderPresent = false;
+      });
+  }
+  onSaveCategory() {
+    this.isLoaderPresent = true;
+    this.http
+      .post(
+        'https://freeapi.gerasim.in/api/BigBasket/CreateNewCategory',
+        this.catObj
+      )
+      .subscribe(
+        (res: any) => {
+          if (res.result) {
+            alert('Category Created Successfully');
+          } else {
+            alert(res.message);
+          }
+          this.isLoaderPresent = false;
+        },
+        (error) => {
+          console.error('Error occurred while saving category:', error);
+          this.isLoaderPresent = false;
+        }
+      );
+  }
 }
