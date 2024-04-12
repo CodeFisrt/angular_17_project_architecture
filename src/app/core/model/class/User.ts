16854b39106d03e75f873db3b3f39d09a587b35f
codefirst2022 <codefirst2022@gmail.com>
2024-04-12 20:45:46 +0530

    Changes

--- src/app/core/model/class/User.ts
+++ src/app/core/model/class/User.ts
@@ -7,3 +7,33 @@ export class User {
         this.mobileNo = '';
     }
 }
+
+export class Product {
+    productId: number;
+    productSku: string;
+    productName: string;
+    productPrice: number;
+    productShortName: string;
+    productDescription: string;
+    createdDate: Date;
+    deliveryTimeSpan: string;
+    categoryId: number;
+    productImageUrl: string;
+    categoryName: string;
+
+    constructor()  {
+        this.categoryId = 0;
+        this.productId = 0;
+        this.categoryName = '';
+        this.createdDate = new Date(),
+        this.deliveryTimeSpan = '';
+        this.productDescription = '';
+        this.productName = '';
+        this.productImageUrl= '';
+        this.productShortName = '';
+        this.productSku = '';
+        this.productPrice = 0;
+    }
+        
+    
+  }
--- src/app/pages/product-list/product-list.component.ts
+++ src/app/pages/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../core/model/class/User';
 
 
 @Component({
@@ -8,93 +9,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
-  ProductList: any [] = [];
-  ProductObj: any = {
-      "productId": 0,
-      "productSku": "",
-      "productName": "",
-      "productPrice": "",
-      "productShortName": "",
-      "productDescription": "",
-      "createdDate": "",
-      "deliveryTimeSpan": "",
-      "categoryId": "",
-      "productImageUrl": "",
-      "categoryName": ""
+  ProductList: any[] = [];
+  ProductObj: Product = new Product();
 
-    };
+  ProductListObj: any[] = [];
 
-    ProductListObj: any[]=[];
-  
-    isLoader: boolean = true;
-    isSaveApiCallInProgress: boolean = false;
-  
-    constructor(private http: HttpClient) {
-      
-    }
-  
-    ngOnInit(): void {
-       this.GetAllProduct();
-    }
-  
-    GetAllProduct() {
-      this.http.get("https://freeapi.gerasim.in/api/BigBasket/GetAllProducts").subscribe((res:any)=>{
-        this.ProductList = res.data;
-        this.isLoader = false
+  isLoader: boolean = true;
+  isSaveApiCallInProgress: boolean = false;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+    this.GetAllProduct();
+  }
+
+  GetAllProduct() {
+    this.http.get("https://freeapi.gerasim.in/api/BigBasket/GetAllProducts").subscribe((res: any) => {
+      this.ProductList = res.data;
+      this.isLoader = false
+    })
+  }
+  onSaveProduct() {
+    debugger;
+    if (this.isSaveApiCallInProgress == false) {
+      this.isSaveApiCallInProgress = true;
+      this.http.post("https://freeapi.gerasim.in/api/amazon/GetAllProducts", this.ProductObj).subscribe((res: any) => {
+        if (res.result) {
+          alert("User Created Success");
+          this.GetAllProduct();
+        } else {
+          alert(res.message)
+        }
+        this.isSaveApiCallInProgress = false;
       })
     }
-    onSaveProduct() {
-      debugger;
-      if(this.isSaveApiCallInProgress == false) {
-        this.isSaveApiCallInProgress =  true;
-        this.http.post("https://freeapi.gerasim.in/api/amazon/GetAllProducts",this.ProductObj).subscribe((res:any)=>{
-          if(res.result) {
-            alert("User Created Success");
-            this.GetAllProduct();
-          } else {
-            alert(res.message)
-          }
-          this.isSaveApiCallInProgress = false;
-        })
-      } 
-    }
-  
-    onDelete(id: number) {
-  
-      const isDelete = confirm("Are you sure want to delete");
-      debugger;
-      if(isDelete == true) {
-        this.http.get("https://freeapi.gerasim.in/api/BigBasket/DeleteProductById?id="+ id).subscribe((res:any)=>{
-          if(res.result) {
-            alert("Product Delete Success");
-            this.GetAllProduct();
-          } else {
-            alert(res.message)
-          } 
-        })
-      }
-  
-     
-    }
-  
-    // GetAllProducts() {
-    //   this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any)=>{
-    //     this.userList =  res;
-    //   })
-    // }
-    // getAllQuestions() {
-    //   this.http.get("https://freeapi.gerasim.in/api/Interview/GetAllQuestions").toPromise().then(
-    //     res => { // Success
-    //       debugger;
-          
-    //     }
-    //   )
-    // }
-    // GetAllProduct2() {
-    //   fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(res=>{
-    //     debugger;
-    //   })
-    // }
-  
   }
-  
+
+  onDelete(id: number) {
+
+    const isDelete = confirm("Are you sure want to delete");
+    debugger;
+    if (isDelete == true) {
+      this.http.get("https://freeapi.gerasim.in/api/BigBasket/DeleteProductById?id=" + id).subscribe((res: any) => {
+        if (res.result) {
+          alert("Product Delete Success");
+          this.GetAllProduct();
+        } else {
+          alert(res.message)
+        }
+      })
+    }
+
+
+  }
+
+  // GetAllProducts() {
+  //   this.http.get("https://jsonplaceholder.typicode.com/users").subscribe((res:any)=>{
+  //     this.userList =  res;
+  //   })
+  // }
+  // getAllQuestions() {
+  //   this.http.get("https://freeapi.gerasim.in/api/Interview/GetAllQuestions").toPromise().then(
+  //     res => { // Success
+  //       debugger;
+
+  //     }
+  //   )
+  // }
+  // GetAllProduct2() {
+  //   fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(res=>{
+  //     debugger;
+  //   })
+  // }
+
+}
