acb63630cb8d47da1b255b72078a7034eae234dd
Vinoth <vinosvpr@gmail.com>
2024-04-16 19:22:02 +0530

    Committing changes on create new product component

--- src/app/pages/create-new-product/create-new-product.component.ts
+++ src/app/pages/create-new-product/create-new-product.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../core/model/class/User';
 
 @Component({
   selector: 'app-create-new-product',
@@ -9,43 +10,22 @@ import { Component, OnInit } from '@angular/core';
 export class CreateNewProductComponent implements OnInit {
   isLoaderPresent: boolean = true;
   productList: any[] = [];
-  productObj: any = {
-    ProductId: 0,
-    ProductSku: '',
-    ProductName: '',
-    ProductPrice: 0,
-    ProductShortName: '',
-    ProductDescription: '',
-    CreatedDate: '2024-04-10T10:58:21.405Z',
-    DeliveryTimeSpan: '',
-    CategoryId: 0,
-    ProductImageUrl: '',
-  };
+  productObj: Product = new Product();
+
+  // productObj: any = {
+  //   ProductId: 0,
+  //   ProductSku: '',
+  //   ProductName: '',
+  //   ProductPrice: 0,
+  //   ProductShortName: '',
+  //   ProductDescription: '',
+  //   CreatedDate: '2024-04-10T10:58:21.405Z',
+  //   DeliveryTimeSpan: '',
+  //   CategoryId: 0,
+  //   ProductImageUrl: '',
+  // };
   constructor(private http: HttpClient) {}
   ngOnInit(): void {}
-  // createProduct() {
-  //   this.http
-  //     .post(
-  //       'https://freeapi.gerasim.in/api/BigBasket/CreateProduct',
-  //       this.productObj
-  //     )
-  //     .subscribe(
-  //       (res: any) => {
-  //         this.productList = res.data;
-  //         if (res.result) {
-  //           alert('Category Created Successfully');
-  //         } else {
-  //           alert(res.message);
-  //         }
-  //         this.isLoaderPresent = false;
-  //       },
-  //       (error) => {
-  //         console.error('Error occurred while creating product:', error);
-  //         this.isLoaderPresent = false;
-  //       }
-  //     );
-  // }
-
   createProduct() {
     this.http
       .post(
@@ -54,30 +34,53 @@ export class CreateNewProductComponent implements OnInit {
       )
       .subscribe(
         (res: any) => {
-          if (res && res.result) {
-            alert('Product Created Successfully');
-            this.productObj = {
-              ProductId: 0,
-              ProductSku: '',
-              ProductName: '',
-              ProductPrice: 0,
-              ProductShortName: '',
-              ProductDescription: '',
-              CreatedDate: '2024-04-10T10:58:21.405Z',
-              DeliveryTimeSpan: '',
-              CategoryId: 0,
-              ProductImageUrl: '',
-            };
+          this.productList = res.data;
+          if (res.result) {
+            alert('Category Created Successfully');
           } else {
-            alert('Failed to create product. ' + res.message);
+            alert(res.message);
           }
           this.isLoaderPresent = false;
         },
         (error) => {
           console.error('Error occurred while creating product:', error);
-          alert('Failed to create product. Please try again later.');
           this.isLoaderPresent = false;
         }
       );
   }
+
+  // createProduct() {
+  //   this.http
+  //     .post(
+  //       'https://freeapi.gerasim.in/api/BigBasket/CreateProduct',
+  //       this.productObj
+  //     )
+  //     .subscribe(
+  //       (res: any) => {
+  //         if (res && res.result) {
+  //           alert('Product Created Successfully');
+  //           this.productObj = {
+  //             ProductId: 0,
+  //             ProductSku: '',
+  //             ProductName: '',
+  //             ProductPrice: 0,
+  //             ProductShortName: '',
+  //             ProductDescription: '',
+  //             CreatedDate: '2024-04-10T10:58:21.405Z',
+  //             DeliveryTimeSpan: '',
+  //             CategoryId: 0,
+  //             ProductImageUrl: '',
+  //           };
+  //         } else {
+  //           alert('Failed to create product. ' + res.message);
+  //         }
+  //         this.isLoaderPresent = false;
+  //       },
+  //       (error) => {
+  //         console.error('Error occurred while creating product:', error);
+  //         alert('Failed to create product. Please try again later.');
+  //         this.isLoaderPresent = false;
+  //       }
+  //     );
+  // }
 }
